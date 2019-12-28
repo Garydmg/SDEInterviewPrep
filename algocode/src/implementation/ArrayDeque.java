007eac0db150964cd557501ffe4f18030412f29b
Daming <daming@umich.edu>
2019-12-27 21:56:59 -0800

    add comment for circular array

--- algocode/src/implementation/ArrayDeque.java
+++ algocode/src/implementation/ArrayDeque.java
@@ -1,30 +1,51 @@
 package implementation;
-
+/**
+ * Data structure implementation for interview: A simple implementation of Java ArrayDeque
+ * --- by Daming Gao, last edited on Dec 27, 2019
+ *
+ * Use fixed-sized circular array to implement Queue and Deque interfaces
+ * Resizable when capacity is reached
+ *
+ * Head and tail are the front and back of the data structure
+ * Semantic Rule:
+ * 1) (head, tail) -> exclusive
+ * 2) tail = head + 1 -> data structure is empty
+ * 3) head = tail -> data structure is full
+ * @param <E>
+ */
 public class ArrayDeque<E> implements Queue<E>, Deque<E> {
     final private int DEFAULT_CAPACITY = 5;
     private int size;
     private int capacity = DEFAULT_CAPACITY;
-    // (head, tail) -> exclusive
-    // empty: tail = head + 1
-    // full: head = tail
     private int head = 0;
     private int tail = head + 1;
-    E[] array;
+    private E[] array;
 
-    // constructor
+    /**
+     * Constructors
+     * 1) Use default capacity
+     * 2) Lower bound is the default capacity
+     */
     ArrayDeque() {
         array = (E[]) new Object[DEFAULT_CAPACITY];
     }
+
     ArrayDeque(int numElements) {
         // lower bound is the default capacity
         capacity = Math.max(numElements, DEFAULT_CAPACITY);
         array = (E[]) new Object[capacity];
     }
 
-    // methods for Queue interface
-    @Override
-    public boolean offer(E e) {
-        // if the array is full
+    /**
+     * Methods for Queue interface (return special values instead of throwing exceptions)
+     * 1) offer: append to the back -> returns true when successful
+     * 2) peek: look at the front -> returns the front element, null when empty
+     * 3) poll: remove from the front -> returns the removed element, null when empty
+     * @param e
+     * @return
+     */
+    @Override public boolean offer(E e) {
+        // if the array is full, expand
         if (head == tail) {
             expandCapacity();
         }
@@ -33,15 +54,15 @@ public class ArrayDeque<E> implements Queue<E>, Deque<E> {
         size++;
         return true;
     }
-    @Override
-    public E peek() {
+
+    @Override public E peek() {
         if (isEmpty()) {
             return null;
         }
         return array[idxForward(head)];
     }
-    @Override
-    public E poll() {
+
+    @Override public E poll() {
         if (isEmpty()) {
             return null;
         }
@@ -52,9 +73,15 @@ public class ArrayDeque<E> implements Queue<E>, Deque<E> {
         return headVal;
     }
 
-    // methods for Deque interface
-    @Override
-    public boolean offerFirst(E e) {
+    /**
+     * Methods for Deque interface (return special values instead of throwing exceptions)
+     * 1) offerFirst/offerLast: append to the back/prepend to the front -> returns true when successful
+     * 2) peekFirst/peekLast: look at the front/back -> returns the front/back element, null when empty
+     * 3) pollFirst/pollLast: remove from the front/back -> returns the removed element, null when empty
+     * @param e
+     * @return
+     */
+    @Override public boolean offerFirst(E e) {
         if (head == tail) {
             expandCapacity();
         }
@@ -63,27 +90,27 @@ public class ArrayDeque<E> implements Queue<E>, Deque<E> {
         size++;
         return true;
     }
-    @Override
-    public boolean offerLast(E e) {
+
+    @Override public boolean offerLast(E e) {
         return offer(e);
     }
-    @Override
-    public E peekFirst() {
+
+    @Override public E peekFirst() {
         return peek();
     }
-    @Override
-    public E peekLast() {
+
+    @Override public E peekLast() {
         if (isEmpty()) {
             return null;
         }
         return array[idxBackward(tail)];
     }
-    @Override
-    public E pollFirst() {
+
+    @Override public E pollFirst() {
         return poll();
     }
-    @Override
-    public E pollLast() {
+
+    @Override public E pollLast() {
         E lastElement = peekLast();
         int last = idxBackward(tail);
         array[last] = null;
@@ -91,7 +118,10 @@ public class ArrayDeque<E> implements Queue<E>, Deque<E> {
         return lastElement;
     }
 
-    // methods common for Queue and Deque interfaces
+    /**
+     * Method common for Queue and Deque interfaces
+     * @return
+     */
     @Override
     public int size() {
         return size;
@@ -101,7 +131,12 @@ public class ArrayDeque<E> implements Queue<E>, Deque<E> {
         return tail == head + 1;
     }
 
-    // private method
+    /**
+     * Private methods
+     * 1) expandCapacity: when capacity is reached, copy the old content to a new array, reset head and tail pointers
+     * 2) idxForward: move pointer to the right, if exceed range (> array.length - 1), wrap around (start from 0)
+     * 3) idxBackward: move pointer to the left, if exceed range (< 0), start from array.length - 1
+     */
     private void expandCapacity() {
         // make a new array of expanded capacity
         capacity = (int) Math.round(capacity * 1.5);
@@ -130,8 +165,10 @@ public class ArrayDeque<E> implements Queue<E>, Deque<E> {
         return (idx - 1 < 0) ? array.length - 1 : idx - 1;
     }
 
-    @Override
-    public void print() {
+    /**
+     * A temporary print function for checking program correctness
+     */
+    @Override public void print() {
         if (isEmpty()) {
             System.out.println("Empty!");
         }
@@ -146,6 +183,10 @@ public class ArrayDeque<E> implements Queue<E>, Deque<E> {
         }
     }
 
+    /**
+     * Driver program for testing
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("---------TEST QUEUE----------");
         Queue<Integer> queue = new ArrayDeque<>(4);
