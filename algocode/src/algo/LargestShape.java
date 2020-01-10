59209c4a4c12a47e092a67508cbaaf50c230bd73
Daming <daming@umich.edu>
2020-01-09 19:40:48 -0500

    largest square of matches

--- algocode/src/algo/LargestShape.java
+++ algocode/src/algo/LargestShape.java
@@ -69,6 +69,70 @@ public class LargestShape {
         return (largestSize == Integer.MIN_VALUE) ? 0 : largestSize;
     }
 
+    /**
+     * Determine the largest square surrounded by a bunch of matches (each match is either horizontal or vertical), return the length of the largest square.
+     * The input is a matrix of points. Each point has one of the following values:
+     * 0 - there is no match to its right or bottom.
+     * 1 - there is a match to its right.
+     * 2 - there is a match to its bottom.
+     * 3 - there is a match to its right, and a match to its bottom.
+     * @param matrix
+     * @return
+     */
+    public int largestSquareOfMatches(int[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+        int numRow = matrix.length;
+        int numCol = matrix[0].length;
+        int[][] right = new int[numRow + 1][numCol + 1];
+        int[][] bottom = new int[numRow + 1][numCol + 1];
+        int result = 0;
+
+        for (int i = numRow - 1; i >= 0; i--) {
+            for (int j = numCol - 1; j >= 0; j--) {
+                // build matrices
+                // 0b01
+                if (hasRight(matrix[i][j])) {
+                    right[i][j] = right[i][j + 1] + 1;
+                }
+                // 0b10
+                if (hasBottom(matrix[i][j])) {
+                    bottom[i][j] = bottom[i + 1][j] + 1;
+                }
+                // 0b11
+                // calculate largest square
+                if (hasBoth(matrix[i][j])) {
+                    // start from the largest value
+                    for (int maxLength = Math.min(right[i][j], bottom[i][j]); maxLength >= 1; maxLength--) {
+                        // do not need to minus one because each coordinate represents a vertex
+                        // e.g. length = 2, 3 vertices
+                        if (bottom[i][j + maxLength] >= maxLength && right[i + maxLength][j] >= maxLength) {
+                            result = Math.max(result, maxLength);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return result;
+    }
+    // "has right" is also the subcategory of "has-both"
+    // both: 0b11
+    // right: 0b01
+    // bottom: 0b10
+    private boolean hasRight(int value) {
+        // only need to check the last bit
+        return (value & 0b1) != 0;
+    }
+    private boolean hasBottom(int value) {
+        // second least significant bit is the same between bottom and both
+        return (value & 0b10) != 0;
+    }
+    private boolean hasBoth(int value) {
+        return value == 0b11;
+    }
+
     private int[][] buildLeftRight(int[][] matrix, int numRow, int numCol) {
         int[][] leftRight = new int[numRow][numCol];
         for (int i = 0; i < numRow; i++) {
@@ -80,8 +144,6 @@ public class LargestShape {
                 }
             }
         }
-        System.out.println("leftRight");
-        print(leftRight, numRow, numCol);
         return leftRight;
     }
     private int[][] buildRightLeft(int[][] matrix, int numRow, int numCol) {
@@ -96,8 +158,6 @@ public class LargestShape {
                 }
             }
         }
-        System.out.println("rightLeft");
-        print(rightLeft, numRow, numCol);
         return rightLeft;
     }
     private int[][] buildTopBottom(int[][] matrix, int numRow, int numCol) {
@@ -112,8 +172,6 @@ public class LargestShape {
                 }
             }
         }
-        System.out.println("topBottom");
-        print(topBottom, numRow, numCol);
         return topBottom;
     }
     private int[][] buildBottomTop(int[][] matrix, int numRow, int numCol) {
@@ -128,8 +186,6 @@ public class LargestShape {
                 }
             }
         }
-        System.out.println("bottomTop");
-        print(bottomTop, numRow, numCol);
         return bottomTop;
     }
 
@@ -144,9 +200,8 @@ public class LargestShape {
 
     public static void main(String[] args) {
         LargestShape sol = new LargestShape();
-        int[][] input = {{1,0,1,1,1},{1,1,1,1,1},{1,1,0,1,0},{1,1,1,1,1}};
-        System.out.println(sol.largestCrossOfOnes(input));
-        System.out.println(sol.largestSquareSurroundedByOne(input));
+        int[][] input = {{3,3,2},{3,3,2},{1,1,0}};
+        System.out.println(sol.largestSquareOfMatches(input));
     }
 
 }
