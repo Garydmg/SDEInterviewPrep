e957839e657242df9e73ac27cd7a52084c40a199
Daming <daming@umich.edu>
2020-01-11 15:10:35 -0500

    add comment and methods for subsum problem

--- algocode/src/algo/SubSum.java
+++ algocode/src/algo/SubSum.java
@@ -1,6 +1,20 @@
 package algo;
 
+/**
+ * "Sub sum problem" - contiguous 1D/2D array sum
+ * Question 1: Largest subarray sum (find a contiguous portion of a 1D array with max sum)
+ * Question 2: Largest submatrix sum (find a rectangle/square portion of a 2D array with max sum)
+ */
 public class SubSum {
+    /**
+     * Question 1:
+     * Largest subarray sum (find a contiguous portion of a 1D array with max sum)
+     * Output the left/right boundaries
+     *
+     * Time: O(n), Space: O(1)
+     * @param nums
+     * @return
+     */
     public int[] largestSubarraySum(int[] nums) {
         if (nums == null || nums.length == 0) {
             return new int[0];
@@ -13,9 +27,12 @@ public class SubSum {
 
         for (int i = 1; i < nums.length; ++i) {
             sum = Math.max(nums[i] + sum, nums[i]);
+            // modify localLeft when we choose num[i] as the beginning of subarray
             if (sum == nums[i]) {
                 localLeft = i;
+                // note: localRight is always i
             }
+            // modify global boundaries only when local sum exceeds global sum
             if (sum > largest) {
                 largest = sum;
                 globalLeft = localLeft;
@@ -24,7 +41,18 @@ public class SubSum {
         }
         return new int[]{largest, globalLeft, globalRight};
     }
-    public int largestSubmatrixSum(int[][] matrix) {
+
+    /**
+     * Question 2:
+     * Compute max submatrix sum
+     * The sum matrix must be a square or rectangle
+     *
+     * Method 1: compute prefix sum beforehand, then look up
+     * Time: O(n^3), Space: O(n^2)
+     * @param matrix
+     * @return
+     */
+    public int largestSubmatrixSumI(int[][] matrix) {
         if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
             return 0;
         }
@@ -36,7 +64,8 @@ public class SubSum {
         int result = prefixRowSum[0][0];
         for (int rowTop = 0; rowTop < numRow; rowTop++) {
             for (int rowBottom = rowTop; rowBottom < numRow; rowBottom++) {
-                result = Math.max(result, maxSubarray(prefixRowSum, rowTop, rowBottom, numCol));
+                int[] array = preprocess(prefixRowSum, rowTop, rowBottom, numCol);
+                result = Math.max(result, maxSubarray(array));
             }
         }
         return result;
@@ -57,18 +86,7 @@ public class SubSum {
         return prefixRowSum;
     }
 
-    private int maxSubarray(int[][] prefixRowSum, int rowTop, int rowBottom, int numCol) {
-        int[] array = preprocess(prefixRowSum, rowTop, rowBottom, numCol);
-        // compute max subarray sum
-        int maxSum = array[0];
-        int localSum = array[0];
-        for (int i = 1; i < array.length; i++) {
-            localSum = Math.max(array[i], localSum + array[i]);
-            maxSum = Math.max(maxSum, localSum);
-        }
-        return maxSum;
-    }
-
+    // a way to "look up" the col sum given rowTop and rowBottom
     private int[] preprocess(int[][] prefixRowSum, int rowTop, int rowBottom, int numCol) {
         int[] array = new int[numCol];
         // compute prefix sum for the row in a column
@@ -83,6 +101,50 @@ public class SubSum {
         return array;
     }
 
+    /**
+     * Question 2:
+     * Method 2: compute prefix sum on the go
+     * Time: O(n^3), Space: O(n)
+     * @param matrix
+     * @return
+     */
+    public int largestSubmatrixSumII(int[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+        int numRow = matrix.length;
+        int numCol = matrix[0].length;
+
+        // initialize result
+        int result = Integer.MIN_VALUE;
+        for (int rowTop = 0; rowTop < numRow; rowTop++) {
+            int[] prefix = new int[numCol];
+            for (int rowBottom = rowTop; rowBottom < numRow; rowBottom++) {
+                // compute prefix sum on the go
+                computePrefix(prefix, matrix[rowBottom]);
+                // compute maxsubarray
+                result = Math.max(result, maxSubarray(prefix));
+            }
+        }
+        return result;
+    }
+
+    private void computePrefix(int[] prefix, int[] cur) {
+        for (int j = 0; j < cur.length; j++) {
+            prefix[j] += cur[j];
+        }
+    }
+
+    private int maxSubarray(int[] array) {
+        int maxSum = array[0];
+        int localSum = array[0];
+        for (int i = 1; i < array.length; i++) {
+            localSum = Math.max(array[i], localSum + array[i]);
+            maxSum = Math.max(maxSum, localSum);
+        }
+        return maxSum;
+    }
+
     private void print(int[] array) {
         for (int i = 0; i < array.length; ++i) {
             System.out.println(array[i]);
@@ -95,6 +157,7 @@ public class SubSum {
         int[] result = sol.largestSubarraySum(array);
         sol.print(result);
         int[][] matrix = {{1,-2,-1,4},{1,-1,1,1},{0,-1,-1,1},{0,0,1,1}};
-        System.out.println(sol.largestSubmatrixSum(matrix));
+        System.out.println(sol.largestSubmatrixSumI(matrix));
+        System.out.println(sol.largestSubmatrixSumII(matrix));
     }
 }
